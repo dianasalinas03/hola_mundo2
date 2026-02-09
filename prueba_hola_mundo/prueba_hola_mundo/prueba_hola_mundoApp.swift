e16de9b4b532d012f7c2b8ab410ceeb32a2cbc23
alumno <diana_salinas03@hotmail.com>
2026-02-09 08:42:33 -0700

    [AGREGA]proyecto

---
+++ prueba_hola_mundo/prueba_hola_mundo/prueba_hola_mundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_hola_mundoApp.swift
+//  prueba_hola_mundo
+//
+//  Created by alumno on 09-02-26.
+//
+
+import SwiftUI
+
+@main
+struct prueba_hola_mundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
